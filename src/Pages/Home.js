f4d850cb922df0e2a29aa27e7d2959f5b8d04615
dnb53 <dnb53@txstate.edu>
2022-04-06 23:17:56 -0500

    display recipes on home page

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,21 +1,33 @@
-import React from 'react';
-import { getAuth } from "firebase/auth";
+import React, { useState, useEffect } from 'react';
+import RecipeCard from '../components/RecipeCard';
+import {db} from "../Firebase/firebase";
+import { collection, getDocs} from "firebase/firestore";
 
 export default function Home() {
 
-  const handleName = () => {
-    const auth = getAuth();
-    const user = auth.currentUser;
+  const [recipes, setRecipes] = useState("");
 
-    if(user != null){
-      console.log(user.displayName);
-    }
+  useEffect(() =>{
+    getData();
+  }, []);
+
+  const getData = async () => {
+    const querySnapshot = await getDocs(collection(db, "Recipes"));
+    querySnapshot.forEach((doc) => {
+    //console.log(doc.data());
+    setRecipes(oldArray => [...oldArray, doc.data()])
+    // console.log("recipes", recipes);
+  });
   }
 
   return (
     <>
-      <div>Home</div>
-      <button onClick={handleName}>Click Me</button>
+      {
+        Object.keys(recipes).map((item, i) => (
+          <RecipeCard key={i} recipe={recipes[item]}/>
+      ))
+
+      }  
     </>
   )
 }
