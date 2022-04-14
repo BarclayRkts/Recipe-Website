56f185392140e8f492fa79aa13683678659f21c8
DeJa Barclay <47193360+BarclayRkts@users.noreply.github.com>
2022-04-13 22:44:23 -0500

    change to HashRouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import { authentication } from "./Firebase/firebase";
 import { onAuthStateChanged } from "firebase/auth";
 import SignOut from "./Pages/Signout";
